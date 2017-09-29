73737d4c8f327b1f1dcbeaa6bcb48fd227789296
Jordan <jordanalex999@hotmail.com>
2017-09-29 15:06:41 -0400

    initial commit

---
+++ page2/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $(".circle1").click(function() {
+    $('body').toggleClass("bgred");
+  });
+})
+$(document).ready(function(){
+  $(".circle2").click(function() {
+    $('body').toggleClass("bgblue");
+  });
+})
+$(document).ready(function(){
+  $(".circle3").click(function() {
+    $('body').toggleClass("bgyellow");
+  });
+})
+$(document).ready(function(){
+  $(".circle4").click(function() {
+    $('body').toggleClass("bggreen");
+  });
+})
+$(document).ready(function(){
+  $(".circle5").click(function() {
+    $('body').toggleClass("bgviolet");
+  });
+})
